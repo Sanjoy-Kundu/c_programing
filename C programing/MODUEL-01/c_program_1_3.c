f6af2a7bfd39cf0d0bc59e55805d60a2d2b2eb41
sanjoy <sanjoykundu187@gamil.com>
2023-04-01 23:50:25 +0600

    c programing new line and tab added

--- C programing/MODUEL-01/c_program_1_3.c
+++ C programing/MODUEL-01/c_program_1_3.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main(){
-	printf("I am a programmer. ");
+	/*
+	printf("Hello World. I am a programmer. ");
+	printf("Niche niche lekh beta");
+	niche niche lekhar jonno amder besh kicu jinish lokkho korte hobe 
+	i. niche niche lekhar joono amder \n = new line dite hobe. 
+	ii. tab dear jonno mader \t = tab  (1 tab \t = 4 space)
+
+	*/
+	printf("Hello world \n");
+	printf("I want to be a programmer\n");
+	printf("Im a Computer Enginner.\t\t\t Im from Madaripur\n Postal code: 7902.");
 	return 0;
 }
