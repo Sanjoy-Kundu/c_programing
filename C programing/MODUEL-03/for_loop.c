c2b869028d053a7cb2feb2bda813982159bb72e9
sanjoy <sanjoykundu187@gamil.com>
2023-04-04 23:40:24 +0600

    moduel_3 added

---
+++ C programing/MODUEL-03/for_loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	/* 
+	For Loop Syntax:
+		for(initilization, condition; increment/decrement){
+			code 
+		}
+	
+	*/
+	int i;
+	for(i = 1; i <= 20; i++){
+		//printf("Hello C programming\n");
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
---
+++ C programing/MODUEL-03/question.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	/*
+	a. loop somporke janbo 
+	b. kon khetre loop babohar korbo seita korbo 
+	c. 1 to select N projonto jog kora shikbo 
+	d. Loop er sathe condition er babohar korbo
+	e. loop ke kivabe controll kora jete pabe . 
+	
+	
+	*/
+}
