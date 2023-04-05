2a52589ad59c126b5d0bbec8293001371f04db66
sanjoy <sanjoykundu187@gamil.com>
2023-04-05 15:55:36 +0600

    continue

---
+++ C programing/MODUEL-03/continue_statement.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+/* 
+* continue er niche jodi kono kicu print dewa thake tahole seita kaj korbe na 
+
+* continue er upor jodi print lekha thake tahole seita kaj korbe orthat je condition thakbe seita bad diye output dekhabe
+
+*/
+
+	int number;
+	for(number = 1; number <= 6; number++){
+
+	/*
+	System-01
+		if(number == 3){
+			continue;
+		}
+		printf("%d\n", number); // aikhane continue kaj korbe 
+*/
+
+printf("%d\n", number);
+	 if(number ==3){
+		continue;  //aikhane continue kaj kobe na
+	 }
+
+
+	}
+
+	return 0;
+}
