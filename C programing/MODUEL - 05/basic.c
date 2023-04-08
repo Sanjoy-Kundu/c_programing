f81db182b03fb3ff6d004172cde94b0695c16b58
sanjoy <sanjoykundu187@gamil.com>
2023-04-09 00:03:42 +0600

    bassic c added

---
+++ C programing/MODUEL - 05/basic.c
@@ -0,0 +1,20 @@
+/* 
+1. c programming er basic setup set korar jonno mara vs code theke chole jabo settings => user snippets ==> c progrmming eee jabo thken c.json e jabo 
+2. google ==> https://snippet-generator.app/ ==> c programing er basic code copy kore paste korbo . 
+3. vs code e basic stracture paste hobe dan side e r akta code asbe seita copy kore  c.json er moddey paste korbo 
+
+
+4.amader practice er jonno akta website ee jete hobe and register korte hobe tar jonno
+google ==> https://codeforces.com/ -> register => registration korlei account create hobe jabe 
+
+
+5. basic stacture er jonno  basic_c likhe enter chaplei chole asbe 
+
+
+*/
+//basic_c
+#include <stdio.h>
+int main(){
+   
+return 0;
+}
